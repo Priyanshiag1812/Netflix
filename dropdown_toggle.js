12d1b457b0b298dc930a856b011ed615d607331c
Priyanshiag1812 <priyanshiagarwal34269@iisuniv.ac.in>
2025-02-21 23:42:20 +0530

    Added Toggle

---
+++ dropdown_toggle.js
@@ -0,0 +1,22 @@
+const icons=document.querySelectorAll(".accord h1 i");
+icons.forEach((icon)=>{
+    icon.addEventListener("click",toggleAccordian);
+});
+
+function toggleAccordian(e){
+    const clickIcon=e.target;
+    const nextpara=clickIcon.parentElement.nextElementSibling;
+
+    if(nextpara.classList.contains("active"))
+        {
+        nextpara.classList.remove("active");
+        clickIcon.classList.remove("fa-square-xmark");
+        clickIcon.classList.add("fa-square-plus")
+        
+    }
+    else{
+        nextpara.classList.add("active");
+        clickIcon.classList.remove("fa-square-plus");
+        clickIcon.classList.add("fa-square-xmark")
+    }
+}
